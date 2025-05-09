2b3b6846a48d5603b57e289af38c38368233f5b9
Nishaadh <adhikarinisha987@gmail.com>
2025-05-09 09:17:59 -0500

    updating code files

--- code/Adjustable Arm/stepper_arm.ino
+++ code/Adjustable Arm/stepper_arm.ino
@@ -21,7 +21,7 @@ Stepper mystepper(stepsPerRevolution, 8, 9, 10, 11);
 
 void setup() {
   // set the speed of the motor to 30 RPMs
-  mystepper.setSpeed(30); //Was 60
+  mystepper.setSpeed(300); //Was 60
   Serial.begin(9600);
   pinMode(button1pin, INPUT);
   pinMode(button2pin, INPUT);
@@ -33,32 +33,44 @@ void loop() {
   //int val = analogRead(0);
   //Serial.println("clockwise");
   //mystepper.step(stepsPerRevolution);
-  delay(500);
+  delay(100);
   //bool button1state = digitalRead(button1pin);
   //bool button2state = digitalRead(button2pin);
   val1 = digitalRead(button1pin);
   val2 = digitalRead(button2pin);
 
-  if (val1 == HIGH) {
-  Serial.println("counterclockwise ");
-  mystepper.step(-stepsPerRevolution);
-  delay(15);
-  val1 = digitalRead(button1pin);
+  while (val1 == HIGH)
+  {
+    Serial.println("counterclockwise ");
+    mystepper.step(-1);
+    delay(15);
+    val1 = digitalRead(button1pin);
   }
 
-  else if (val2 == HIGH){
-  Serial.println("clockwise ");
-  mystepper.step(stepsPerRevolution);
-  delay(15);
-  val2 = digitalRead(button2pin);
+  while (val2 == HIGH) {
+    Serial.println("clockwise ");
+    mystepper.step(1);
+    delay(15);
+    val2 = digitalRead(button2pin);
   }
 
-  else {
-  Serial.println("stop ");
-  mystepper.step(0);
-  val1 = digitalRead(button1pin);
-  val2 = digitalRead(button2pin); 
-  delay(500);
+  /*else if (val2 == HIGH){
+    Serial.println("clockwise ");
+    mystepper.step(stepsPerRevolution);
+    delay(15);
+    val2 = digitalRead(button2pin);
+  }*/
+
+  if (!val1 || !val2) {
+    Serial.println("stop ");
+    /*mystepper.step(0);
+    val1 = digitalRead(button1pin);
+    val2 = digitalRead(button2pin); */
+    delay(15);
+    digitalWrite(8,LOW);
+    digitalWrite(9,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(11,LOW);
   }
 
 //Serial.println("clockwise ");
--- code/Electronic Wheel/wheelcontrol.ino
+++ code/Electronic Wheel/wheelcontrol.ino
@@ -3,6 +3,16 @@ bool brake = 0;
 int x = 0;
 int y = 0;
 
+const int buttonpin = 2;
+int buttonstate = 0;
+int presscount = 0;
+int speed1 = 5;
+int speed2 = 50 ;
+
+int lastButtonState = LOW;   
+unsigned long lastDebounceTime = 0;  
+unsigned long debounceDelay = 50;  
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -18,23 +28,67 @@ void loop() {
 
   y = analogRead(A4);
   x = analogRead(A5);
-  speed = 10 ;
+;
+  int b_read = digitalRead(buttonpin);
+
+  // If the button state has changed
+  if (b_read != lastButtonState) {
+    lastDebounceTime = millis();  // Reset the debounce timer
+  }
+
+  // If the state has been stable longer than debounceDelay
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (b_read != buttonstate) {
+      buttonstate = b_read;
+
+      // Only toggle the LED when the new state is HIGH (button pressed)
+      if (buttonstate == HIGH) {
+        presscount = (presscount == 2) ? 0 : presscount + 1;
+      }
+    }
+  }
+
+  lastButtonState = b_read;
+
+  //if (buttonstate){
+  //    presscount = (presscount == 2) ? 0 : presscount + 1;
+  //  }
 
   if (y < 400){
     digitalWrite(7, LOW);
     digitalWrite(4, LOW);
-    analogWrite(9, speed);
+    if (presscount == 0)
+    {
+      digitalWrite(7, HIGH);
+    }
+    else if (presscount == 1){
+      analogWrite(9, speed1);
+    }
+    else if (presscount == 2){
+      analogWrite(9, speed2);
+    }
   }
 
   else if (y > 600){
     digitalWrite(7, LOW);
     digitalWrite(4, HIGH);
-    analogWrite(9, speed);
+    if (presscount == 0)
+    {
+      digitalWrite(7, HIGH);
+    }
+    else if (presscount == 1){
+      analogWrite(9, speed1);
+    }
+    else if (presscount == 2){
+      analogWrite(9, speed2);
+    }
   }
 
   else {
     digitalWrite(7, HIGH);
   }
+
+  Serial.println(presscount);
 
 
 }
